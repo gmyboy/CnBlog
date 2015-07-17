40c689c69c3c7231d524f3679ccf9ca4e0d6d4a7
gmyboy <2275964276@qq.com>
2015-07-17 15:21:07 +0800

    add the comment
    add the comment with the smooth Switch

--- app/src/main/java/com/gmy/cnblog/util/StringUtil.java
+++ app/src/main/java/com/gmy/cnblog/util/StringUtil.java
@@ -1,5 +1,11 @@
 package com.gmy.cnblog.util;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Point;
+import android.view.Display;
+import android.view.WindowManager;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -19,5 +25,36 @@ public class StringUtil {
         Date date = forsdf.parse(temp1);
         String now = latsdf.format(date);
         return now + "" + temp2;
+    }
+
+    private static int screenWidth = 0;
+    private static int screenHeight = 0;
+
+    public static int dpToPx(int dp) {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+    }
+
+    public static int getScreenHeight(Context c) {
+        if (screenHeight == 0) {
+            WindowManager wm = (WindowManager) c.getSystemService(Context.WINDOW_SERVICE);
+            Display display = wm.getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            screenHeight = size.y;
+        }
+
+        return screenHeight;
+    }
+
+    public static int getScreenWidth(Context c) {
+        if (screenWidth == 0) {
+            WindowManager wm = (WindowManager) c.getSystemService(Context.WINDOW_SERVICE);
+            Display display = wm.getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            screenWidth = size.x;
+        }
+
+        return screenWidth;
     }
 }
